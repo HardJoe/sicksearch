8925991270747004cb4e06fa9ea397b12d9bbec3
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2022-12-07 20:00:52 +0700

    Change pickle to dill and put result to SERP

--- home/search.py
+++ home/search.py
@@ -1,10 +1,12 @@
-from bsbi import BSBIIndex
-from compression import VBEPostings
+import os
+
+from home.bsbi import BSBIIndex
+from home.compression import VBEPostings
 
 # sebelumnya sudah dilakukan indexing
 # BSBIIndex hanya sebagai abstraksi untuk index tersebut
 BSBI_instance = BSBIIndex(
-    data_dir="collection", postings_encoding=VBEPostings, output_dir="home\\search\\index"
+    data_dir=os.path.join("home", "collection"), postings_encoding=VBEPostings, output_dir=os.path.join("home", "indices")
 )
 
 queries = [
@@ -13,6 +15,13 @@ queries = [
     "lipid metabolism in toxemia and normal pregnancy",
 ]
 for query in queries:
+    print("Query   : ", query)
+    print("Results :")
+    for (score, doc) in BSBI_instance.retrieve_bm25(query, k=10):
+        print(f"{doc:30} {score:>.3f}")
+    print()
+
+def test_search(query):
     print("Query   : ", query)
     print("Results :")
     for (score, doc) in BSBI_instance.retrieve_bm25(query, k=10):
--- home/views.py
+++ home/views.py
@@ -1,44 +1,43 @@
 from django.shortcuts import render
 
-# import os
+import os
 
-# from django.conf import settings
+from django.conf import settings
 
-# from .bsbi import BSBIIndex
-# from .compression import VBEPostings
-
-
+from home.bsbi import BSBIIndex
+from home.compression import VBEPostings
+from home.search import test_search
 
 
 def index(request):
     return render(request, 'index.html')
 
 def search(request):
-    # if 'q' in request.GET:
-    #     query = request.GET['q']
-    #     docs = get_serp(query)
-    #     context = {'docs': docs}
+    if 'q' in request.GET:
+        query = request.GET['q']
+        docs = get_serp(query)
+        context = {'docs': docs}
+        print(docs)
 
-    # return render(request, 'results.html', context=context)
-    return render(request, 'results.html')
+    return render(request, 'results.html', context=context)
+    #     test_search(query)
+    # return render(request, 'results.html')
 
 
 
-# def get_serp(query):
-#     # sebelumnya sudah dilakukan indexing
-#     # BSBIIndex hanya sebagai abstraksi untuk index tersebut
-#     BSBI_instance = BSBIIndex(
-#         data_dir="collection", postings_encoding=VBEPostings, output_dir=os.path.join('index', 'index')
-#     )
+def get_serp(query):
+    BSBI_instance = BSBIIndex(
+        data_dir=os.path.join("home", "collection"), postings_encoding=VBEPostings, output_dir=os.path.join("home", "indices")
+    )
 
-#     docs = [{'path': doc} for (_, doc) in BSBI_instance.retrieve_bm25(query, k=10)]
-#     for doc in docs:
-#         with open(doc['path']) as f:
-#             title = f.readline()
-#             title = (title[:65] + ' ...') if len(title) > 69 else title
-#             doc['title'] = title
+    docs = [{'path': doc} for (_, doc) in BSBI_instance.retrieve_bm25(query, k=10)]
+    for doc in docs:
+        with open(doc['path']) as f:
+            title = f.readline()
+            title = (title[:65] + ' ...') if len(title) > 69 else title
+            doc['title'] = title
 
-#             content = f.read()
-#             content = (content[:161] + ' ...') if len(content) > 165 else content
-#             doc['content'] = content
-#     return docs
+            content = f.read()
+            content = (content[:161] + ' ...') if len(content) > 165 else content
+            doc['content'] = content
+    return docs
